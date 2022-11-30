401b5c59dba19657733fa96d16ec5825699d1987
Ahzam Khan <ahzam.main@gmail.com>
2022-11-30 22:54:46 +0530

    max token fix

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,7 +15,7 @@ const generateAction = async (req, res) => {
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
     temperature: 0.9,
-    max_tokens: 300,
+    max_tokens: 250,
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
