cfe26ab8a5c76e02a01bad72fd7d01664d252cd9
Ahzam Khan <ahzam.main@gmail.com>
2022-11-30 22:45:41 +0530

    responsive

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import {
   Flex,
@@ -41,23 +42,27 @@ export default function home() {
 
   return (
     <div className="main">
-      <Flex justify={"center"}>
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
+      <Flex justify={"center"} overflowY={"hidden"}>
         <Flex
           flexDir={"column"}
-          marginTop={"45px"}
+          marginTop={{base:"25px",md:"45px"}}
           className={"blur"}
-          width={"1086px"}
+          height={{base:"90vh",md:"707px"}}
+          width={{base:"90vw",md:"1086px"}}
           borderRadius={"20px"}
-          height={"707px"}
           background={"rgba(0,0 , 0, 0.3)"}
           border={"2px solid rgba(255, 255, 255, 0.3)"}
           align={"center"}
+          marginBottom={{base:"40px",md:"0"}}
         >
           <ButtonGroup
             color={"rgba(255, 255, 255, 0.6)"}
             className={"blur"}
             marginTop={"34.68px"}
-            width={"465px"}
+            width={{base:"300px",md:"465px"}}
             borderRadius={"20px"}
             height={"72.52px"}
             background={"rgba(0,0 , 0, 0.3)"}
@@ -75,7 +80,7 @@ export default function home() {
                 onChange={onUserChangedText}
                 _placeholder={{ color: "rgba(255, 255, 255, 0.4)" }}
                 fontWeight={"semibold"}
-                fontSize={"24px"}
+                fontSize={{base:"19px",md:"24px"}}
                 background={"None"}
                 placeholder={"Enter a topic"}
               />
@@ -95,14 +100,17 @@ export default function home() {
             color={"rgba(255, 255, 255, 0.6)"}
             fontWeight={"500"}
             marginTop={"30px"}
-            width={"800px"}
+            width={{base:"80vw",md:"800px"}}
             textAlign={"left"}
-            fontSize={"24px"}
+            fontSize={{base:"19px",md:"24px"}}
             marginLeft={""}
             overflowY={"auto"}
             paddingBottom={"30px"}
           >
             {apiOutput}
+            Once upon a time in a distant galaxy, there lived a brave princess named Nakita. She was the daughter of a legendary space explorer called Arlon. Nakita dreamed of one day exploring the universe, just like her father had done. So, when her father offered to take her on a journey to explore a mysterious wormhole, she jumped at the chance. Little did Nakita know that the adventure she was about to embark on would end up saving the entire universe. The journey to the wormhole began. For what seemed like an eternity, they traveled through the galaxies, seeing sights that most people had never seen before. But when they arrived at their destination, they were stunned. The wormhole was enormous, so large that their tiny spaceship could have fit inside of it. They took the risk and entered the wormhole. Inside, they experienced something that they had never experienced before—time travel. By using the laws of physics, they were able to travel through time to different galaxies and explore them. As they traveled, Nakita and Arlon discovered that the wormhole was powerful enough to allow them to create a portal to different galaxies and times. With the new knowledge, Nakita and Arlon were able to save the universe from a deadly enemy—a powerful space monster known as the Dreaded. Using the wormhole, Nakita and Arlon were able to travel back in time and seal away the Dreaded, saving the universe from disaster. Now that the universe was saved, Nakita and Arlon returned home with all the knowledge they had gained. With their newfound skills, they could explore the universe to their hearts' content. Nakita and Arlon's epic adventure had come to an end, but their bravery and courage would never be forgotten, and the wormhole would be remembered as one
+
+
            
           </Text>
         </Flex>
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import { Flex, Text, Image, Button } from "@chakra-ui/react";
 export default function Home() {
   return (
     <div className="main">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Flex
         fontFamily={"Manrope, sans-serif"}
         color={"white"}
@@ -12,12 +15,11 @@ export default function Home() {
         <Flex
           className="blur"
           height={"88px"}
-          font
           background={"rgba(0, 0, 0, 0.1)"}
           width={"100%"}
           justifyContent={"space-between"}
         >
-          <Flex marginLeft={"58px"} gap={"5px"}>
+          <Flex marginLeft={{ base: "38px", md: "58px" }} gap={"5px"}>
             <Image
               src="book-open.svg"
               marginTop={"32px"}
@@ -25,73 +27,78 @@ export default function Home() {
               height={"25px"}
               alt="logo"
             />
-            <Text fontWeight={"bold"} fontSize={"24px"} marginTop={"26px"}>
+            <Text
+              fontWeight={"bold"}
+              fontSize={{ base: "20px", md: "24px" }}
+              marginTop={"26px"}
+            >
               {" "}
               eli5skool{" "}
             </Text>
           </Flex>
+          <a href="https://twitter.com/0xpakhi" target={"blank"}>
           <Text
-            marginRight={"58px"}
+              marginRight={{base:"38px",md:"58px"}}
             fontWeight={"bold"}
             marginTop={"26px"}
-            fontSize={"24px"}
+            fontSize={{ base: "20px", md: "24px" }}
           >
-            {" "}
-            memo{" "}
+            twitter
           </Text>
+          </a>
         </Flex>
 
         <Flex flexDir={"column"} marginTop={"180px"} align={"center"}>
           <Text
             textAlign={"center"}
-            width={"715px"}
-            fontSize={"70px"}
+            width={{ base: "315px", md: "715px" }}
+            fontSize={{ base: "35px", md: "70px" }}
             fontWeight={"extrabold"}
           >
             so, what are we learning today?
           </Text>
           <a href={"./home"}>
-          <Button
-            background={"white"}
-            color={"black"}
-            width={"183px"}
-            height={"59px"}
-            marginTop={"45px"}
-            align={"center"}
-            fontSize={"24px"}
-            fontWeight={"semibold"}
-            borderRadius={"100px"}
-          >
-            try out eli5
-          </Button>
+            <Button
+              background={"white"}
+              color={"black"}
+              width={{ base: "150px", md: "183px" }}
+              height={{ base: "49px", md: "59px" }}
+              marginTop={"45px"}
+              align={"center"}
+              fontSize={{ base: "20px", md: "24px" }}
+              fontWeight={"semibold"}
+              borderRadius={"100px"}
+            >
+              try out eli5
+            </Button>
           </a>
         </Flex>
 
-          <a href="https://buildspace.so" target={"blank"}>
-
-        <Button
-          marginBottom={"36px"}
-          marginLeft={"42px"}
-          width={"184px"}
-          height={"50px"}
-          bottom={"0px"}
-          position={"fixed"}
-          background={"rgba(255, 255, 255, 0.0.5)"}
-          border={"1px solid rgba(255, 255, 255, 0.3)"}
-          borderRadius={"100px"}
-          align={"center"}
-          justify={"center"}
-          _hover={{ background: "rgba(0, 0, 0, 0.3)" }}
-        >
-          <Text
-            color={"rgba(255, 255, 255, 0.5)"}
-            fontWeight={"semibold"}
-            fontSize={"16px"}
+        <a href="https://buildspace.so" target={"blank"}>
+          <Button
+            display={{ base: "None", md: "flex" }}
+            marginBottom={"36px"}
+            marginLeft={"42px"}
+            width={"184px"}
+            height={"50px"}
+            bottom={"0px"}
+            position={"fixed"}
+            background={"rgba(255, 255, 255, 0.0.5)"}
+            border={"1px solid rgba(255, 255, 255, 0.3)"}
+            borderRadius={"100px"}
+            align={"center"}
+            justify={"center"}
+            _hover={{ background: "rgba(0, 0, 0, 0.3)" }}
           >
-            built at buildspace
-          </Text>
-        </Button>
-          </a>
+            <Text
+              color={"rgba(255, 255, 255, 0.5)"}
+              fontWeight={"semibold"}
+              fontSize={"16px"}
+            >
+              built at buildspace
+            </Text>
+          </Button>
+        </a>
       </Flex>
     </div>
   );
